69fad2cbaeb1cc1fb64d6de6f7c24b4c592a2462
Shafiqur Rahman <pallab.alif@gmail.com>
2023-01-08 11:59:15 +0600

    ADD: View Model Data Binding and Factory.

--- app/src/main/java/com/srpallab/viewmodelexample/MainActivity.kt
+++ app/src/main/java/com/srpallab/viewmodelexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.srpallab.viewmodelexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.srpallab.viewmodelexample.databinding.ActivityMainBinding
@@ -9,10 +10,30 @@ import com.srpallab.viewmodelexample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this ).get(MainActivityViewModel::class.java)
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(
+            this, viewModelFactory
+        )[MainActivityViewModel::class.java]
+
+        binding.tvResult.text = viewModel.getNumber().toString()
+
+        binding.btnAdd.setOnClickListener {
+            if(binding.etNumber.text.isNotEmpty()){
+                val numberToAdd: String = binding.etNumber.text.toString()
+                viewModel.addNumber(numberToAdd.toInt())
+                binding.tvResult.text = viewModel.getNumber().toString()
+            } else{
+                Toast.makeText(
+                    this, "Please Give a Number", Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/srpallab/viewmodelexample/MainActivityViewModel.kt
+++ app/src/main/java/com/srpallab/viewmodelexample/MainActivityViewModel.kt
@@ -2,6 +2,18 @@ package com.srpallab.viewmodelexample
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel(startingTotal: Int) : ViewModel() {
+    private var result: Int = 0
 
+    init {
+        result = startingTotal
+    }
+
+    fun addNumber(number: Int) {
+        result += number
+    }
+
+    fun getNumber(): Int {
+        return result
+    }
 }
